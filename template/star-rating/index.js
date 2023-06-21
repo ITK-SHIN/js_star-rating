5dba90d7a8e68895a1b63cca66a7507ec28249b5
신상우 <bano112@naver.com>
2023-06-21 23:20:11 +0900

    'star-rating요소개수지정리팩토링'

--- template/star-rating/index.js
+++ template/star-rating/index.js
@@ -9,34 +9,25 @@ lastLink.after(newLink);
 newLink.setAttribute('href', 'star-rating/theme.css');
 newLink.setAttribute('rel', 'stylesheet');
 lastLink.after(newLink);
-
 /*  star-rating 요소 내부의 css를 JavaScript로 자동 추가하기  끝*/
+
 
 const StarRating = ($container) => {
   //console.log($container); -> 출력시 [div.star-rating, div.star-rating]
 
   //요구4 :  star-rating 요소의 data-max-rating 어트리뷰트를 통해 동적으로 생성할
   //star 요소의 갯수를 지정한다.
+const maxRating = $container.dataset['maxRating'];
+const starContainer = document.createElement('div');
+starContainer.classList.add('star-rating-container');
 
-  if ($container.dataset['maxRating'] === '3') {
-    $container.innerHTML = `
-                <div class=star-rating-container>
-                <i class='bx bxs-star' ></i>
-                <i class='bx bxs-star' ></i>
-                <i class='bx bxs-star' ></i>
-                </div>
-                `;
-  } else {
-    $container.innerHTML = `
-                <div class=star-rating-container>
-                <i class='bx bxs-star' ></i>
-                <i class='bx bxs-star' ></i>
-                <i class='bx bxs-star' ></i>
-                <i class='bx bxs-star' ></i>
-                <i class='bx bxs-star' ></i>
-                </div>
-                `;
-  }
+for (let i = 0; i < maxRating; i++) {
+  const star = document.createElement('i');
+  star.classList.add('bx', 'bxs-star');
+  starContainer.appendChild(star);
+}
+
+$container.appendChild(starContainer);
 
   /****************************  기능 5, 6 구현 시작 ********************** */
   /****************************  기능 7,8 구현시작 *********************** */
